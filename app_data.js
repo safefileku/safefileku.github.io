746dbf1f5215c1113df4d9c62efdd9c88078e66a
safefileku <162449211+safefileku@users.noreply.github.com>
2024-03-06 20:26:51 +0800

    Update app_data.js

--- app_data.js
+++ app_data.js
@@ -5,20 +5,36 @@ const appData = [
     // Tambahkan data aplikasi lainnya sesuai kebutuhan
 ];
 
-// Fungsi untuk menampilkan data aplikasi di halaman web
+// Fungsi untuk menampilkan data aplikasi berdasarkan hasil pencarian
 function tampilkanDataAplikasi() {
     const appListContainer = document.getElementById('app-list');
+    const startIdx = (currentPage - 1) * itemsPerPage;
+    const endIdx = startIdx + itemsPerPage;
 
-    appData.forEach(app => {
-        const appItem = document.createElement('div');
-        appItem.classList.add('app-item');
-        appItem.innerHTML = `
-            <strong>Nama:</strong> ${app.nama}<br>
-            <strong>Ukuran:</strong> ${app.ukuran}<br>
-            <button onclick="downloadApp('${app.linkDownload}')">Tombol Download</button>
-        `;
-        appListContainer.appendChild(appItem);
-    });
+    // Reset konten sebelumnya
+    appListContainer.innerHTML = '';
+
+    // Tampilkan data aplikasi sesuai hasil pencarian
+    let searchQuery = document.getElementById('appName').value.toLowerCase();
+
+    const filteredApps = appData.filter(app => app.nama.toLowerCase().includes(searchQuery));
+
+    if (filteredApps.length > 0) {
+        for (let i = startIdx; i < Math.min(endIdx, filteredApps.length); i++) {
+            const appItem = document.createElement('div');
+            appItem.classList.add('app-item');
+            appItem.innerHTML = `
+                <strong>Nama:</strong> ${filteredApps[i].nama}<br>
+                <strong>Ukuran:</strong> ${filteredApps[i].ukuran}<br>
+                <button onclick="downloadApp('${filteredApps[i].linkDownload}')">Tombol Download</button>
+            `;
+            appListContainer.appendChild(appItem);
+        }
+    } else {
+        const noResultMessage = document.createElement('p');
+        noResultMessage.textContent = 'Tidak ada hasil yang ditemukan.';
+        appListContainer.appendChild(noResultMessage);
+    }
 }
 
 // Fungsi untuk mengakses tautan unduhan
@@ -26,6 +42,3 @@ function downloadApp(linkDownload) {
     // Redirect atau lakukan aksi yang sesuai dengan kebutuhan aplikasi
     window.location.href = linkDownload;
 }
-
-// Panggil fungsi untuk menampilkan data aplikasi
-tampilkanDataAplikasi();
