dbcd2627542b8a7c3780db002398c241c3f8e53a
safefileku <162449211+safefileku@users.noreply.github.com>
2024-03-06 23:40:28 +0800

    Update app_data.js

--- app_data.js
+++ app_data.js
@@ -7,6 +7,19 @@ const appData = [
     { nama: "Mivo_v3.26.565_Premium", ukuran: "108.83 MB", linkDownload: "https://safefileku.com/download/Pqe1DoTnl1BTAboL" },
     { nama: "Puffin_Cloud_Browser_9.9.0.51519_51519200_ca2a67", ukuran: "32.43 MB", linkDownload: "https://safefileku.com/download/NupX4rwDXcPysFS7" },
     { nama: "Reface_3.33.0_348_789f73", ukuran: "57.95 MB", linkDownload: "https://safefileku.com/download/UjkGQruc9ZaTIMBs" },
+    { nama: "ZEDGE v7.39.0 MOD", ukuran: "32.60 MB", linkDownload: "https://safefileku.com/download/j5LRHjjBgXnDYpp" },
+    { nama: "WPS Office v16.3 MOD", ukuran: "106.13 MB", linkDownload: "https://safefileku.com/download/BVoRafirRN4S578" },
+    { nama: "YouCut v1.516.1142 MOD", ukuran: "48 MB", linkDownload: "https://safefileku.com/download/k0BpSHZkm7m1Ikl" },
+    { nama: "Wps Wpa Tester Premium v5.0.2", ukuran: "9.82 MB", linkDownload: "https://safefileku.com/download/ebRxI03w2TsxPF0" },
+    { nama: "WhatsApp Messenger v2.22.13.6", ukuran: "38.69 MB", linkDownload: "https://safefileku.com/download/qYmRTErhvH8G7Rd" },
+    { nama: "World History Atlas v3.38 PAID", ukuran: "10.59 MB", linkDownload: "https://safefileku.com/download/wzwNBbJoRt6EmA3" },
+    { nama: "VSCO v272 MOD", ukuran: "83.93 MB", linkDownload: "https://safefileku.com/download/8n1qS1L6zMv2UoS" },
+    { nama: "VivaCut v2.13.5 MOD", ukuran: "111.80 MB", linkDownload: "https://safefileku.com/download/VhoYCCSUDPTJ6qb" },
+    { nama: "VMOS PRO v2.3.4 MOD", ukuran: "26.81 MB", linkDownload: "https://safefileku.com/download/4FlfCwM4IO8KOoh" },
+    { nama: "VIU Premium v1.1.14 MOD", ukuran: "20.79 MB", linkDownload: "https://safefileku.com/download/QUEsqMt7tPuH6S2" },
+    { nama: "Videoder v14.5 MOD", ukuran: "9.51 MB", linkDownload: "https://safefileku.com/download/BtglBCwzmN0yYuh" },
+    { nama: "Vanced Manager v2.6.2", ukuran: "4.26 MB", linkDownload: "https://safefileku.com/download/AOVTI7NhImf7eU5" },
+    
     // Tambahkan data aplikasi lainnya sesuai kebutuhan
 ];
 
